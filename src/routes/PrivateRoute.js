2eaf4049b8c2cdbf27cc8587781ea3ab32670d8e
Jang-YuJin <jangyj0912@naver.com>
2026-02-11 12:54:00 +0900

    level -> lvl 수정

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -5,7 +5,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const PrivateRoute = ({ permissionLevel }) => {
   const user = useSelector((state) => state.user.user);
   const isAuthenticated =
-    user?.level === permissionLevel || user?.level === "admin";
+    user?.lvl === permissionLevel || user?.lvl === "admin";
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
